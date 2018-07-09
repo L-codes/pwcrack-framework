81eecbdd0202f298b7b6fb4c845e950a68469aac
L <l-codes@qq.com>
2018-07-09 13:52:54 +0800

    add chamd5 plugin

---
+++ plugins/chamd5.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+#
+# Plugin chamd5
+# Author L
+# Date   2018-07-09
+#
+
+plugin 'chamd5' do 
+  web_server 'http://www.chamd5.org/'
+  supported_algorithm :md5, :sha1, :mysql3, :mysql
+
+  crack {
+    data = {'email': 'nkitvg69570@chacuo.net','pass': '5c2f2b01245f43f9a5f703a96a6ac4a6','type': 'login'}
+    post_json '/HttpProxyAccess.aspx/ajax_login', data
+    enum_algorithm do |algorithm|
+      type = case algorithm
+             when :md5
+               'md5'
+             when :sha1
+               '100'
+             when :mysql3
+               '200'
+             when :mysql
+               '300'
+             end
+      data = {'hash': passwd, 'type': type}
+      r = post_json '/HttpProxyAccess.aspx/ajax_me1ody', data
+      extract(r.body, /\\"result\\":\\"(.+?)\\"}/)
+    end
+  }
+end
+
