bb182a38d6b6c9be193ca358430473329bf7c9ce
L <l-codes@qq.com>
2018-10-19 13:42:57 +0800

    plugin chamd5 add lm&ntlm algorithm

--- plugins/chamd5.rb
+++ plugins/chamd5.rb
@@ -7,14 +7,14 @@
 
 plugin 'chamd5' do 
   web_server 'http://www.chamd5.org/'
-  supported_algorithm :md5, :sha1, :mysql3, :mysql
+  supported_algorithm :md5, :sha1, :mysql3, :mysql, :lm, :ntlm
 
   crack {
     data = {'email': 'nkitvg69570@chacuo.net','pass': '5c2f2b01245f43f9a5f703a96a6ac4a6','type': 'login'}
     post_json '/HttpProxyAccess.aspx/ajax_login', data
     enum_algorithm do |algorithm|
       type = case algorithm
-             when :md5
+             when :md5, :lm, :ntlm
                'md5'
              when :sha1
                '100'
