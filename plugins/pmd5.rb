d3059867b52fcf78652b72717818b1649c69a06b
L <l-codes@qq.com>
2018-11-11 18:15:12 +0800

    Update plugins/pmd5.rb

--- plugins/pmd5.rb
+++ plugins/pmd5.rb
@@ -5,19 +5,12 @@
 #
 
 plugin 'pmd5' do 
-  web_server 'http://pmd5.com'
+  web_server 'http://api.pmd5.com'
   supported_algorithm :md5, :md5_16
 
   crack {
-		r = get '/'
-    unless r.body.empty?
-      info = Hash[r.body.scan(/id="(__[\w]+)" value="(.*?)"/)]
-			data = {"__VIEWSTATE": info["__VIEWSTATE"], "__EVENTVALIDATION": info["__EVENTVALIDATION"],
-							"__VIEWSTATEGENERATOR": info["__VIEWSTATEGENERATOR"],
-							"key": passwd, "jiemi": "MD5\u89e3\u5bc6"}
-      r = post('/', data)
-      r.body.extract(/#{passwd}(?:.*?)<em>(.+?)<\/em>/)
-    end
+		r = get '/pmd5api/pmd5', {'pwd': passwd}
+    r.body.extract(/:"(.*?)"}}/)
   }
 end
 
