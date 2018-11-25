3a81af02519222effcd35a67625b97708bbbd801
L <l-codes@qq.com>
2018-11-25 14:17:17 +0800

    Add somd5 Plugin

---
+++ plugins/somd5.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+#
+# Plugin so_md5
+# Author L
+#
+
+plugin "somd5" do 
+  web_server "https://www.somd5.com"
+  supported_algorithm :md5, :md5_16, :md4, :sha1, :sha256, :sha384, :sha512, :ntlm, :mysql3, :mysql 
+
+  crack {
+    r = post '/search.php', {hash: passwd, captcha: 0}
+    r.body.extract(/"data":"(.*?)"/)
+  }
+end
+
