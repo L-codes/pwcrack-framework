56a0fa326c3b10cbb4d4b0149885c4226d60d122
L <l-codes@qq.com>
2018-11-25 22:34:36 +0800

    Add somd5 whirlpool algo

--- plugins/somd5.rb
+++ plugins/somd5.rb
@@ -6,7 +6,7 @@
 
 plugin "somd5" do 
   web_server "https://www.somd5.com"
-  supported_algorithm :md5, :md5_16, :md4, :sha1, :sha256, :sha384, :sha512, :ntlm, :mysql3, :mysql 
+  supported_algorithm :md5, :md5_16, :md4, :sha1, :sha256, :sha384, :sha512, :ntlm, :mysql3, :mysql, :whirlpool
 
   crack {
     r = post '/search.php', {hash: passwd, captcha: 0}
