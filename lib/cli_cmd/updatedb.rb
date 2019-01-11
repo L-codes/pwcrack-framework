d2b181e71d0e8a66eea231ac0b765b33dc47885b
L <l-codes@qq.com>
2019-01-11 23:00:33 +0800

    Update mysql5 -> hex(sha1(sha1(word)))

--- lib/cli_cmd/updatedb.rb
+++ lib/cli_cmd/updatedb.rb
@@ -20,14 +20,6 @@ module CLI
       :mysql => {},
     }
 
-    begin 
-      require 'mysql_make_scrambled_password'
-    rescue LoadError
-      puts "[*] Need to support MySQL hash: `gem install mysql_make_scrambled_password`"
-      puts
-      hashes.delete :mysql
-    end
-
     md5 = OpenSSL::Digest::MD5.new
     sha1 = OpenSSL::Digest::SHA1.new
     words = open(word_file).each(chomp:true).to_a
@@ -42,6 +34,7 @@ module CLI
 
     words.each_with_index do |word, i|
 
+      sha1b = sha1.digest(word)
       md5_1 = md5.digest(word)
       md5_2 = md5.digest(md5_1)
       md5_3 = md5.hexdigest(md5_2)
@@ -67,10 +60,10 @@ module CLI
           hashes[algo][ md5_3_hex[8,16].hex2int ] = i
           hashes[algo][ md5_3_hex_up[8,16].hex2int ] = i
         when :sha1
-          hashes[algo][ sha1.hexdigest(word)[10,16].hex2int ] = i
+          hashes[algo][ sha1b[5,8].bytes2int ] = i
         when :mysql
-          mysql_hash = MysqlMakeScrambledPassword.make_scrambled_password(word)
-          hashes[algo][ mysql_hash[11,16].hex2int ] = i
+          mysql_hash = sha1.hexdigest(sha1b)
+          hashes[algo][ mysql_hash[10,16].hex2int ] = i
         end
       end
 
@@ -88,7 +81,6 @@ module CLI
       algo = "#{name}".center 7
 
       puts "[+] %s db (%7s) : [%4.1fM] data/db/%s.bin" % [algo.bold, obj.size, msize, name]
-      #puts "[+] local #{algo.bold} db (#{obj.size.to_s.rjust(7)}) : data/db/#{name}.bin"
       msize
     end
 
