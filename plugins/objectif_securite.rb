c51b60b0c72ddfa503bcdae972409fddc17855da
L <l-codes@qq.com>
2019-10-17 12:22:56 +0800

    update plugins/objectif_securite.rb

--- plugins/objectif_securite.rb
+++ plugins/objectif_securite.rb
@@ -5,12 +5,12 @@
 #
 
 plugin 'objectif_securite' do 
-  web_server 'https://www.objectif-securite.ch'
+  web_server 'https://cracker.okx.ch:8443'
   supported_algorithm :ntlm
 
   crack {
     data = {"value": passwd}
-    r = post_json '/demo.php/crack', data
+    r = post_json '/crack', data
     if r.body !~ /Password not found| ?queue\.? /i
       r.body.extract(/"msg":"(.+?)"/)
     end
