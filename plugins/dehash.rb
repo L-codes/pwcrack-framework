52c68d1ec1fa8074b8210d8d4dcd87253f7d6010
L <l-codes@qq.com>
2020-03-26 12:54:31 +0800

    update plugins/dehash.rb

--- plugins/dehash.rb
+++ plugins/dehash.rb
@@ -8,13 +8,13 @@ plugin 'dehash' do
   web_server 'https://dehash.me'
   supported_algorithm *%i[
     md2 md4 md5 mdc2 ripemd128 ripemd160 ripemd256
-    ripmd320 sha1 sha224 sha256 sha384 sha512 whirlpool
+    ripemd320 sha1 sha224 sha256 sha384 sha512 whirlpool
   ]
 
   crack {
     get '/'
-    r = post '/', "input-text=#{passwd}&dehash-button="
-    r.body.extract(%r|<th>Hash Algorithm</th></tr><tr><td>(.*?)</td>|)
+    r = post '/', "searchText=#{passwd}&dehash=Dehash+me"
+    r.body.extract(%r|<th scope="col">Hash Algorithm</th></tr>\s*<tr>\s*<td>(.*?)</td>|)
   }
 end
 
