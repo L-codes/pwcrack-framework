d257d5a83535c207d5f406a5c7f347632ce2d0ea
L <l-codes@qq.com>
2022-05-10 10:24:58 +0800

    Add openssl decryption method

--- plugins/landray_ekp.rb
+++ plugins/landray_ekp.rb
@@ -5,20 +5,25 @@
 # File: WEB-INF/KmssConfig/admin.properties
 #
 
-if Kernel.const_defined? :Rjb
-
 plugin 'landray_ekp' do 
   supported_algorithm :landray_ekp
 
   crack {
-    EKPDESEncrypt = Rjb.import 'com.landray.kmss.util.DESEncrypt'
-    plain = ""
-    [false, true].find { |israndom|
-      des = EKPDESEncrypt.new("kmssAdminKey", israndom)
-      plain = des.decrypt(passwd.hex2ascii) rescue nil
-    }
+    key = 'kmssAdminKey'
+
+    if Kernel.const_defined? :Rjb
+      EKPDESEncrypt = Rjb.import 'com.landray.kmss.util.DESEncrypt'
+      plain = ""
+      [false, true].find { |israndom|
+        des = EKPDESEncrypt.new(key, israndom)
+        plain = des.decrypt(passwd.hex2ascii) rescue nil
+      }
+
+    else
+      plain = algo_decrypt('des-ecb', msg: passwd.hex2ascii, key: key[0, 8])
+
+    end
+
     plain if plain&.printable?
   }
-end
-
 end
